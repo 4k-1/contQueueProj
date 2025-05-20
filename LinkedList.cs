fb13dbcbb323bae8303785c1c7e20607424ab02e
4k-1 <149543027+4k-1@users.noreply.github.com>
2025-05-20 23:57:42 +0800

    Add files via upload

---
+++ LinkedList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LinkedList
+{
+    public class Node
+    {
+        public int Data;
+        public Node Next;
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+```
+private Node head;
+    private int currentNumber;
+    private int count;
+
+    public LinkedList(int startNumber)
+    {
+        currentNumber = startNumber;
+        head = null;
+    }
+
+    public int Enqueue()
+    {
+        Node newNode = new Node(currentNumber++);
+        if (head == null)
+            head = newNode;
+        else
+        {
+            Node temp = head;
+            while (temp.Next != null)
+                temp = temp.Next;
+            temp.Next = newNode;
+        }
+        count++;
+        return newNode.Data;
+    }
+
+    public int? Dequeue()
+    {
+        if (head == null)
+            return null;
+
+        int data = head.Data;
+        head = head.Next;
+        count--;
+        return data;
+    }
+
+    public int? Peek()
+    {
+        return head?.Data;
+    }
+
+    public int PeekLast()
+    {
+        if (head == null) return -1;
+        Node temp = head;
+        while (temp.Next != null)
+            temp = temp.Next;
+        return temp.Data;
+    }
+
+    public int Count
+    {
+        get { return count; }
+    }
+
+    public bool IsEmpty()
+    {
+        return head == null;
+    }
+
+
+}
+
---
+++ QueueMonitorForm.cs
@@ -0,0 +1,179 @@
+namespace queueProject
+{
+    public partial class QueueMonitorForm : Form
+
+    {
+        private LinkedList depositQueue;
+        private LinkedList loanQeue;
+        private LinkedList accountQueue;
+        private LinkedList customerQueue;
+
+        public QueueMonitorForm()
+        {
+
+            InitializeComponent();
+
+            depositQueue = new LinkedList(1000);
+            loanQueue = new LinkedList(2000);
+            accountQueue = new LinkedList(3000);
+            customerQueue = new LinkedList(4000);
+
+
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //deposit/Withdrawal
+            depositQueue.Enqueue();
+            int number = depositQueue.PeekLast();
+            heresNumlbl.Text = number.ToString();
+
+
+        }
+        private void panel6_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Account
+            accountQueue.Enqueue();
+            int number = accountQueue.PeekLast();
+            heresNumlbl.Text = number.ToString(); 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //customer
+            customerQueue.Enqueue();
+            int number = customerQueue.PeekLast();
+            heresNumlbl.Text = number.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //loan 
+            loanQueue.Enqueue();
+            int number = loanQueue.PeekLast();
+            heresNumlbl.Text = number.ToString();
+        }
+
+
+        private void c2NSLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void c1NSLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void c3NSLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void c4NSLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void c1CallBtn_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        int? currentServingNumber = null;
+
+        private void c1NextBtn\_Click(object sender, EventArgs e)
+        {
+            if (depositQueue.Count > 0)
+            {
+                int? nextNumber = depositQueue.Dequeue();
+                currentServingNumber = nextNumber;
+
+
+                c1NSLabel.Text = nextNumber.ToString(); // this will display on the counter
+            }
+            else
+            {
+                c1NSLabel.Text = "";
+            }
+
+
+        }
+
+        int? currentServingNumberC2 = null;
+
+        private void c2NextBtn\_Click(object sender, EventArgs e)
+        {
+            if (loanQueue.Count > 0)
+            {
+                int? nextNumber = loanQueue.Dequeue();
+                currentServingNumberC2 = nextNumber;
+
+
+                c2NSLabel.Text = nextNumber.ToString();
+            }
+            else
+            {
+                c2NSLabel.Text = "";
+            }
+
+        }
+
+        int? currentServingNumberC3 = null;
+
+        private void c3NextBtn\_Click(object sender, EventArgs e)
+        {
+            if (accountQueue.Count > 0)
+            {
+                int? nextNumber = accountQueue.Dequeue();
+                currentServingNumberC3 = nextNumber;
+
+
+                c3NSLabel.Text = nextNumber.ToString();
+            }
+            else
+            {
+                c3NSLabel.Text = "";
+            }
+
+
+        }
+
+        int? currentServingNumberC4 = null;
+
+        private void c4NextBtn\_Click(object sender, EventArgs e)
+        {
+            if (customerQueue.Count > 0)
+            {
+                int? nextNumber = customerQueue.Dequeue();
+                currentServingNumberC4 = nextNumber;
+
+
+                 c4NSLabel.Text = nextNumber.ToString();
+            }
+            else
+            {
+                c4NSLabel.Text = "";
+            }
+
+
+         }
+       
+    }
